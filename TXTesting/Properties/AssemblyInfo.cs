a1cda54eae72e080c4c3083b269eabbfd1b6289c
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2020-05-18 18:54:51 +0200

    uloženo přes VS

---
+++ TXTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TXTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TXTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("104de5f4-2911-4398-b843-c9673b6dd123")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TXTesting/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TXT;
+
+namespace TXTesting
+{
+    [TestClass]
+    public class TXTesting
+    {
+        IsText IT;
+        public TXTesting()
+        {
+            IT = new IsText();
+        }
+        [TestMethod]
+        public void IsTextEmpty()
+        {
+            string s = MainWindow.TextRTB;
+            bool result = IT.ProhlizeTextIsTrueFalse(s);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void IsTextFound()
+        {
+            string a = MainWindow.TextRTB;
+            string b = NajitText.textVOkne;
+            bool result = IT.TextNalezenTrueFalse(a, b);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void IsTimeAdded()
+        {
+            string a = MainWindow.TextRTB;
+            string b = MainWindow.NewRTB;
+            bool result = IT.CasPridanTrueFalse(a, b);
+            Assert.IsTrue(result);
+        }
+    }
+}
